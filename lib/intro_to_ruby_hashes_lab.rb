652b9622c6e77b8f226de6417064863c4a9a6948
Thomas Bigelow <tjbigelow@att.net>
2020-09-06 23:41:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,8 @@
+require "pry"
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   {key: value}
+  binding.pry
 end
 
 def read_from_hash(hash, key)
